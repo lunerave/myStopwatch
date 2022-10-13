22cce5bb5bd83a4cfad50bdba5b3a67b0e7e0805
이상록 <lune@pc-10-32.customer.ask4.lan>
2022-10-13 17:20:07 +0200

    build start and pause button switch when click and stopwatch start and pause.

--- app/src/main/java/com/example/mystopwatch/MainActivity.kt
+++ app/src/main/java/com/example/mystopwatch/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.mystopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mystopwatch.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var time = 0
+    var timerTask: Timer?=null
+    var isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener{
+            isRunning =! isRunning
+
+            if(isRunning){
+                start()
+            }else{
+                pause()
+            }
+        }
     }
+
+    fun start() {
+        binding.fab.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24)
+
+        timerTask = timer(period = 10) {
+            time++
+            val sec = time / 100
+            val milli = time % 100
+
+            runOnUiThread{
+                binding.secView.text = "$sec"
+                binding.milliSecView.text = "$milli"
+            }
+
+
+        }
+    }
+
+    fun pause() {
+        binding.fab.setImageResource(R.drawable.ic_baseline_play_circle_filled_24)
+        timerTask?.cancel()
+    }
+
+
 }
